f796e7cef27add8199726feb19467b992b1b6b20
Lucas Charbonnier <lcs.charbonnier@gmail.com>
2020-06-12 21:30:02 +0200

    Improved some functions, added new ones and added comments to the functions.

--- rng_utils.cpp
+++ rng_utils.cpp
@@ -1,46 +1,58 @@
+#include <random>
+#include <chrono>
+
 #include "rng_utils.h"
 
 namespace rng_utils {
-	int rand_int(int lowerInc, int upperInc) {
+	int rnd_int(int lowerInc, int upperInc) {
 		static std::default_random_engine rng{ static_cast<unsigned int>(std::chrono::system_clock::now().time_since_epoch().count()) };
 		std::uniform_int_distribution<int> dist(lowerInc, upperInc);
 		return dist(rng);
 	}
 
-	float rand_normalized_float(int precision) {
-		return static_cast<float>(rand_int(-precision, precision)) / static_cast<float>(precision);
+	float rnd_float_normalized(int precision) {
+		return static_cast<float>(static_cast<float>(rnd_int(-precision, precision))) / static_cast<float>(precision);
 	}
 
-	float rand_float(float min, float max) {
-		auto val = ((min + max) + rand_normalized_float() * (max - min)) / 2.f;
+	float rnd_float(float min, float max) {
+		auto val = ((min + max) + rnd_float_normalized() * (max - min)) / 2.f;
 		return val;
 	}
 
-	float random_angle_deg() {
-		return rand_float(0.f, 360.f);
+	float rnd_angle_deg() {
+		return rnd_float(0.f, 360.f);
 	}
 
-	float random_angle_rad() {
+	float rnd_angle_rad() {
 		static constexpr float PI_2 = 2.f * 3.1415926f;
-		return rand_float(0.f, PI_2);
+		return rnd_float(0.f, PI_2);
 	}
 
 #ifdef USE_SFML
-	sf::Vector2f random_vector(float minXY, float maxXY) {
-		return sf::Vector2f(rand_float(minXY, maxXY), rand_float(minXY, maxXY));
+	sf::Vector2f rnd_sfvector(float minX, float maxX, float minY, float maxY) {
+		return sf::Vector2f(rnd_float(minX, maxX), rnd_float(minY, maxY));
 	}
 
-	sf::Vector2f random_unit_direction() {
-		float randomRadianAngle = random_angle_rad();
+	sf::Vector2f rnd_unit_sfvector() {
+		float randomRadianAngle = rnd_angle_rad();
 		return sf::Vector2f(std::cosf(randomRadianAngle), std::sinf(randomRadianAngle));
 	}
 
-	sf::Vector2f random_point_within_circle(float circleRadius) {
-		return random_unit_direction() * rand_float(0.f, circleRadius);
+	sf::Vector2f rnd_point_on_rect(sf::Vector2f topLeftCorner, sf::Vector2f size) {
+		auto bottomRightCorner = topLeftCorner + size;
+		return rnd_sfvector(topLeftCorner.x, topLeftCorner.y, bottomRightCorner.x, bottomRightCorner.y);
 	}
 
-	sf::Vector2f random_point_inside_donut(float innerRadius, float outerRadius) {
-		return random_unit_direction() * rand_float(innerRadius, outerRadius);
+	sf::Vector2f rnd_point_on_rect(sf::Vector2f center, float width, float height) {
+		return center + sf::Vector2f(rnd_float_normalized(width / 2.f), rnd_float_normalized(height / 2.f));
+	}
+
+	sf::Vector2f rnd_point_on_circle(float circleRadius, sf::Vector2f circleCenter) {
+		return circleCenter + rnd_unit_sfvector() * rnd_float(0.f, circleRadius);
+	}
+
+	sf::Vector2f rnd_point_on_torus(float innerRadius, float outerRadius, sf::Vector2f torusCenter) {
+		return torusCenter + rnd_unit_sfvector() * rnd_float(innerRadius, outerRadius);
 	}
 #endif // USE_SFML
 }
--- rng_utils.h
+++ rng_utils.h
@@ -1,26 +1,45 @@
 #pragma once
 
-#include <random>
-#include <chrono>
-
-// Uncomment the following USE_SFML #define if sfml is linked to your project.
-//#define USE_SFML
+// Uncomment the USE_SFML #define if sfml is linked to your project.
+#define USE_SFML
 
 #ifdef USE_SFML
 #include <SFML/System/Vector2.hpp>
 #endif // USE_SFML
 
 namespace rng_utils {
-	int rand_int(int lowerInc, int upperInc);
-	float rand_normalized_float(int precision = 10000000);
-	float rand_float(float min, float max);
-	float random_angle_deg();
-	float random_angle_rad();
+	// Generates a random integer between the given boundaries.
+	int rnd_int(int lowerInc, int upperInc);
+
+	// Generates a random normalized float (between -1 and 1). The "precision" tells how small the gap between 2 randomly generated numbers can be (the bigger the precision, the smaller the gap). 
+	float rnd_float_normalized(int precision = 10000000);
+
+	// Generates a random float within the given boundaries.
+	float rnd_float(float min, float max);
+
+	// Generates a float representing an angle in degrees (between 0 and 360).
+	float rnd_angle_deg();
+
+	// Generates a float representing an angle in radians (between 0 and 2*pi).
+	float rnd_angle_rad();
 
 #ifdef USE_SFML
-	sf::Vector2f random_vector(float minXY, float maxXY);
-	sf::Vector2f random_unit_direction();
-	sf::Vector2f random_point_within_circle(float circleRadius);
-	sf::Vector2f random_point_inside_donut(float innerRadius, float outerRadius);
+	// Generates a vector with random XY values between the given boundaries.
+	sf::Vector2f rnd_sfvector(float minX, float maxX, float minY, float maxY);
+
+	// Generates a random unit vector.
+	sf::Vector2f rnd_unit_sfvector();
+
+	// Generates a random point located within the given rectangle.
+	sf::Vector2f rnd_point_on_rect(sf::Vector2f topLeftCorner, sf::Vector2f size);
+
+	// Generates a random point located within the given rectangle.
+	sf::Vector2f rnd_point_on_rect(sf::Vector2f center, float width, float height);
+
+	// Generates a random point located within the given circle.
+	sf::Vector2f rnd_point_on_circle(float circleRadius, sf::Vector2f circleCenter = { 0.f,0.f });
+
+	// Generates a random point located on the given taurus (taurus = donut).
+	sf::Vector2f rnd_point_on_torus(float innerRadius, float outerRadius, sf::Vector2f torusCenter = { 0.f,0.f });
 #endif // USE_SFML
 }
